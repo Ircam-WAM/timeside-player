12c7921fe10aa196713afcf287272e5aa7eec31c
Martin Desrumaux <martin.desrumaux@ircam.fr>
2020-04-01 16:00:13 +0200

    fix(formatResponseError): Handle unknown error (e.g. CORS Request failed)

--- src/utils/response-error.ts
+++ src/utils/response-error.ts
@@ -1,3 +1,8 @@
 export function formatResponseError (resp: Response) {
-  return `Invalid response from the API: ${resp.statusText} (${resp.status})`
+  if (resp.statusText || resp.status) {
+    return `Invalid response from the API: ${resp.statusText} (${resp.status})`
+  } else {
+    console.error('unknwown error: ', resp)
+    return 'Unknown error from the API. Please check the console and network logs of your browser.'
+  }
 }
