3e6a0f17be916914296221f5a491ff166b08ea9b
Martin Desrumaux <martin.desrumaux@ircam.fr>
2020-04-18 19:56:01 +0200

    fix(Region): Use svg's path for close icon for consistency across browsers

--- src/components/Region.vue
+++ src/components/Region.vue
@@ -25,17 +25,31 @@
       height="100%"
       @mousedown.stop="startResizeRight"
     />
-    <text
-      v-if="width > 25"
-      :x="stop - 25"
+    <svg
+      v-if="width > 40"
+      :x="stop - 40"
       :y="0"
-      dominant-baseline="hanging"
-      font-size="40"
+      width="40"
+      height="40"
+      viewBox="0 0 512 512"
       class="close"
       @mousedown.stop="closeHandler"
     >
-      &times;
-    </text>
+      <!-- Use an empty rect so click event is on the whole svg -->
+      <rect
+        x="0"
+        y="0"
+        width="100%"
+        height="100%"
+        fill="transparent"
+      />
+      <!-- center -->
+      <g transform="scale(0.5) translate(256 256)">
+        <path
+          d="M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4  L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1  c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z"
+        />
+      </g>
+    </svg>
   </g>
 </template>
 
