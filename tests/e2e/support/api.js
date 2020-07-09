373e3a4f96308f5451851d21d1af853a6ae5c2fe
Martin Desrumaux <martin@letik.io>
2020-07-09 17:59:47 +0200

    feat(cypress): mustGetEnv

--- tests/e2e/support/api.js
+++ tests/e2e/support/api.js
@@ -1,6 +1,16 @@
-export const API_BASE = Cypress.env('TIMESIDE_BASE_URL')
-export const API_USER = Cypress.env('TIMESIDE_USER')
-export const API_PASS = Cypress.env('TIMESIDE_PASS')
+function mustGetEnv (key) {
+  const val = Cypress.env(key)
+  if (!val) {
+    throw new Error(`"${key}" is not available in environment. Check README.md`)
+  }
+  return val
+}
+
+export const API_BASE = mustGetEnv('TIMESIDE_BASE_URL')
+export const API_USER = mustGetEnv('TIMESIDE_USER')
+export const API_PASS = mustGetEnv('TIMESIDE_PASS')
+
+if (!API_BASE)
 
 // set api-url in local storage to let app know which API URL it should use
 beforeEach(() => {
