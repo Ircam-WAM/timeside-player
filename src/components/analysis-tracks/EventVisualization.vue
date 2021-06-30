f270715ec3d4a584138e4e5cc447e8ad467aa3df
rherbelleau <romain.herbelleau@centrale-marseille.fr>
2021-06-30 18:38:50 +0200

    add unit on global time-mode, add event time-mode,and fix framewiseVisualization

---
+++ src/components/analysis-tracks/EventVisualization.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="event-container">
+    <div
+      v-for="{ val, idx } of points"
+      :key="val"
+      :style="{ left: `${val}%`, height: `${values[idx] * 100}%` }"
+      class="event"
+    />
+  </div>
+</template>
+
+<script lang='ts'>
+import {
+  defineComponent,
+  PropType,
+  computed
+} from '@vue/composition-api'
+
+import { HDF5 } from './HDF5Event'
+
+export default defineComponent({
+  props: {
+    hdf5: {
+      type: Object as PropType<HDF5>,
+      required: true
+    },
+    start: {
+      type: Number,
+      required: false
+    },
+    stop: {
+      type: Number,
+      required: false
+    }
+  },
+  setup (props) {
+    const duration = computed(() => props.hdf5.audio_metadata.duration)
+
+    const start = computed(() => props.start || 0)
+    const stop = computed(() => props.stop || duration.value * 1000)
+
+    const points = computed(() => props.hdf5.data_object.time.numpyArray
+      .map((x, idx) => {
+        const t = x * 1000
+        if (t > start.value && t < stop.value) {
+          return {
+            val: (t - start.value) / (stop.value - start.value) * 100,
+            idx
+          }
+        }
+      })
+      .filter((x) => x !== undefined)
+    )
+
+    const maxVal = Math.max(...props.hdf5.data_object.label.numpyArray)
+    const values = computed(() => props.hdf5.data_object.label.numpyArray.map((x) => {
+      return x / maxVal
+    }))
+
+    return {
+      points,
+      values
+    }
+  }
+})
+</script>
+
+<style lang="less" scoped>
+.event-container {
+  position: relative;
+  height: 100%;
+}
+.event {
+  background-color: black;
+  width: 2px;
+  position: absolute;
+}
+
+</style>
--- src/components/analysis-tracks/HDF5Visualization.vue
+++ src/components/analysis-tracks/HDF5Visualization.vue
@@ -13,9 +13,16 @@
         :stop="stop"
       />
     </template>
+    <template v-else-if="hdf5.time_mode === 'event'">
+      <EventVisualization
+        :hdf5="hdf5"
+        :start="start"
+        :stop="stop"
+      />
+    </template>
     <template v-else-if="hdf5.time_mode === 'global'">
       <div>
-        {{ hdf5.id_metadata.name }} = {{ hdf5.data_object.value.numpyArray[0] }}
+        {{ hdf5.id_metadata.name }} = {{ hdf5.data_object.value.numpyArray[0] }} {{ hdf5.id_metadata.unit }}
       </div>
     </template>
     <template v-else-if="hdf5.time_mode === 'timewise'">
@@ -37,6 +44,7 @@ import {
 
 import { HDF5, basePath } from '@/utils/api'
 import FramewiseVisualization from '@/components/analysis-tracks/FramewiseVisualization.vue'
+import EventVisualization from './EventVisualization.vue'
 
 import { formatResponseError } from '@/utils/response-error'
 
@@ -56,7 +64,8 @@ export default defineComponent({
     }
   },
   components: {
-    FramewiseVisualization
+    FramewiseVisualization,
+    EventVisualization
   },
   setup (props) {
     const loading = ref(true)
