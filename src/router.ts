9b9ea98c81052568b216df38967786702c6edce6
gllmp <guillaume.piccarreta@gmail.com>
2022-06-30 10:42:20 +0200

    set app view as router default

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,14 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-import ListView from '@/views/List.vue'
+import App from '@/views/App.vue'
+// import ListView from '@/views/List.vue'
 import ItemView from '@/views/Item.vue'
 
 export default createRouter({
   history: createWebHashHistory(),
   routes: [
-    { path: '/', name: 'list', component: ListView },
+    // { path: '/', name: 'list', component: ListView },
+    { path: '/', name: 'app', component: App },
     { path: '/item/:id', name: 'item', component: ItemView }
   ]
 })
